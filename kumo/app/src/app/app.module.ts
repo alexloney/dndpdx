0590574d18b4b149cd6a4859b4466f01c2ffa066
Alex Loney <loney.alex@gmail.com>
2019-09-13 13:20:02 -0700

    Updating to be a bit more variable and better looking

--- kumo/app/src/app/app.module.ts
+++ kumo/app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,10 @@ import { HomeComponent } from './home/home.component';
 // PrimeNG Imports
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -22,9 +27,15 @@ import { ButtonModule } from 'primeng/button';
     FormsModule,
     InputTextModule,
     ButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    DialogModule,
+    BrowserAnimationsModule,
+    ProgressSpinnerModule,
+    ConfirmDialogModule
   ],
-  providers: [],
+  providers: [
+    ConfirmationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- kumo/app/src/app/home/home.component.ts
+++ kumo/app/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ConfirmationService } from 'primeng/api';
 import { DatabaseService } from './../database.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,15 +10,33 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public userid;
+  public displayLogin = false;
+  public loading = false;
 
-  constructor(private ds: DatabaseService) { }
+  constructor(private ds: DatabaseService,
+    private confirmationService: ConfirmationService) { }
 
   ngOnInit() {
   }
 
+  public showLogin() {
+    this.displayLogin = true;
+  }
+
   public loginWithId(e) {
+    this.loading = true;
     this.ds.isIdValid(this.userid).subscribe((success) => {
+      this.confirmationService.confirm({
+        message: 'Message',
+        accept: () => {
+          // Stuff
+        }
+      });
       console.log(success);
+    }, (failure) => {
+      console.error(failure);
+    }, () => {
+      this.loading = false;
     });
   }
 
