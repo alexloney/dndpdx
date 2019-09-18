f832bc3afc4ed7d48066a9180e30b45a64398719
Alex Loney <loney.alex@gmail.com>
2019-09-17 20:12:17 -0700

    Adding different environment/endpoints for dev/prod

--- kumo/app/src/app/database.service.ts
+++ kumo/app/src/app/database.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
-  private endpoint = 'http://127.0.0.1:5002/';
+  private endpoint = environment.apiUrl;
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
